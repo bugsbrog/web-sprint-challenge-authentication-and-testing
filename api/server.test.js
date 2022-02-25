db106da757f5ccc00ed8ccf6976c04285e229484
bugsbrog <bugsbrog@gmail.com>
2022-02-25 08:58:41 -0700

    Writing tests now. 2 tests done

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,23 @@
-// Write your tests here
+const request = require('supertest')
+const db = require('../data/dbConfig')
+const server = require('./server')
+
 test('sanity', () => {
   expect(true).toBe(true)
 })
+
+test('NODE_ENV is correct', () => {
+  expect(process.env.NODE_ENV).toBe('testing')
+})
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+beforeEach(async () => {
+  await db('users').truncate()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+
